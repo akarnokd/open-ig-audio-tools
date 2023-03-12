269d3e4889088592158baa1658653df083addddd
akarnokd <akarnokd@gmail.com>
2023-03-12 19:01:48 +0100

    Finish format

--- XMF_Converter/XMFFile.cs
+++ XMF_Converter/XMFFile.cs
@@ -11,11 +11,18 @@ namespace XMF_Converter
 {
     public class XMFFile
     {
-        public byte version;
+        public int version;
 
         public readonly List<SampleRegistry> sampleRegistry = new();
 
         public readonly List<byte> sectionIndexes = new();
+
+        public int sampleCount;
+
+        public int sectionCount;
+
+        // Not sure what these are, one per sample.
+        public readonly List<byte> controlFlags = new();
 
         public readonly List<InstructionSection> instructionSections = new();
 
@@ -23,13 +30,72 @@ namespace XMF_Converter
         {
             version = reader.ReadByte();
 
+            for (int i = 0; i < 256; i++)
+            {
+                var regEntry = new SampleRegistry();
+                regEntry.LoadFrom(reader);
+                if (regEntry.Length != 0)
+                {
+                    sampleRegistry.Add(regEntry);
+                }
+            }
 
+            bool endMarkerFound = false;
+            for (int i = 0; i < 256; i++)
+            {
+                var b = reader.ReadByte();
+                if (!endMarkerFound)
+                {
+                    if (b != 255)
+                    {
+                        sectionIndexes.Add(b);
+                    }
+                    else
+                    {
+                        endMarkerFound = true;
+                    }
+                }
+            }
+
+            sampleCount = reader.ReadByte() + 1;
+            
+            sectionCount = reader.ReadByte() + 1;
+
+            for (int i = 0; i < sampleCount; i++)
+            {
+                controlFlags.Add(reader.ReadByte());
+            }
+
+            for (int sectionIndex = 0; sectionIndex < sectionCount; sectionIndex++)
+            {
+                var section = new InstructionSection();
+                instructionSections.Add(section);
+
+                for (int i = 0; i < 0x180; i++)
+                {
+                    var row = new InstructionRow();
+                    section.rows.Add(row);
+
+                    for (int j = 0; j < sampleCount; j++)
+                    {
+                        var instr = new Instruction();
+                        row.columns.Add(instr);
+
+                        instr.LoadFrom(reader);
+                    }
+                }
+            }
+
+            foreach (var regEntry in sampleRegistry)
+            {
+                reader.Read(regEntry.sampleBytes);
+            }
         }
     }
 
     public class SampleRegistry
     {
-        public long offset0;
+        public long offset1;
         public long offset2;
         public long offset3;
         public long offset4;
@@ -41,7 +107,26 @@ namespace XMF_Converter
 
         public ushort frequency;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public byte[] sampleBytes;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public void LoadFrom(BinaryReader br)
+        {
+            offset1 = Read3Bytes(br);
+            offset2 = Read3Bytes(br);
+            offset2 = Read3Bytes(br);
+            offset4 = Read3Bytes(br);
+            param0 = br.ReadByte();
+            param1 = br.ReadByte();
+            frequency = br.ReadUInt16();
+            sampleBytes = new byte[Length];
+        }
+
+        private long Read3Bytes(BinaryReader br)
+        {
+            return ((long)br.ReadUInt16()) + br.ReadByte() * 65536;
+        }
     }
 
     public class InstructionSection
@@ -51,6 +136,7 @@ namespace XMF_Converter
 
     public class InstructionRow
     {
+        // per sample instructions
         public readonly List<Instruction> columns = new();
     }
 
@@ -62,6 +148,16 @@ namespace XMF_Converter
         public byte func2;
         public byte func2_Param;
         public byte func1_Param;
+
+        public void LoadFrom(BinaryReader br)
+        {
+            note = br.ReadByte();
+            sampleNumber = br.ReadByte();
+            func1 = br.ReadByte();
+            func2 = br.ReadByte();
+            func2_Param = br.ReadByte();
+            func1_Param = br.ReadByte();
+        }
     }
 
 }
